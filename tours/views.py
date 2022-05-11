f16184ac0fe2ec996fe5b3348e7d7fa33b58c7a4
Sudo debug <sudodebug@gmail.com>
2022-05-11 09:47:39 +0500

    create views welcome

--- tours/views.py
+++ tours/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-
+from django.http import Http404
 
 def index(request):
     return render(request, 'tours/index.html')
@@ -8,3 +8,29 @@ def index(request):
 def about(request):
     return render(request, 'tours/about.html')
 
+
+def welcome(request):
+    week = 5
+    course = 'django'
+    group = 12345
+    return render(request, 'tours/welcome.html', context={
+        'week': week,
+        'course': course,
+        'group': group,
+    })
+
+
+def student(request, student_id):
+    students = {
+        1: 'alex',
+        2: 'anna',
+        3: 'oleg',
+    }
+    # try:
+    # student = students[student_id]
+    # except KeyError:
+    #     raise Http404('wow')
+
+    return render(request, 'tours/welcome.html', context={
+        'student': student,
+    })
